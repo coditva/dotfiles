32b70b2e3756417ada73793de3ced7f408b1c9cb
UTkarsh <utkarshme96@gmail.com>
2017-07-09 16:52:09 +0530

    Alias hub as git

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,3 +3,8 @@ alias open="xdg-open"
 
 # update the system with one command
 alias sysupdate="sudo apt-get update && sudo apt-get dist-upgrade --show-progress && sudo apt-get autoremove && sudo apt-get clean && echo Done"
+
+# if hub is installed, alias it as git
+if which hub > /dev/null; then
+    alias git=hub
+fi
