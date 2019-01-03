c13adf609d15ce2838adef2c39997807e2fa789f
Utkarsh Maheshwari <utkarshme96@gmail.com>
2019-01-04 00:33:49 +0530

    Remove system update command for Ubuntu

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,9 +1,6 @@
 # make opening easy
 alias open="xdg-open"
 
-# update the system with one command
-alias sysupdate="sudo apt-get update && sudo apt-get dist-upgrade --show-progress && sudo apt-get autoremove && sudo apt-get clean && echo Done"
-
 # if hub is installed, alias it as git
 if which hub > /dev/null; then
     alias git=hub
